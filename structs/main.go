3b544630fa6f63f9b89a90b5a6839a436b08eb5f
michaelzheng1 <mzheng1@terpmail.umd.edu>
2022-04-20 18:50:55 -0400

    adds pointer

--- structs/main.go
+++ structs/main.go
@@ -2,15 +2,34 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	//alex := person{"Alex", "Anderson"}
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jim.updateName("jimmy")
+	jim.print()
+}
 
-	fmt.Println(alex)
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
 
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
